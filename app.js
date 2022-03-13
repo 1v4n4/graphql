1ce234b3a2175201871f682295182a2a3e59c366
1v4n4 <ivananl@yahoo.com>
2022-03-13 11:42:03 +0100

    Add and resololve object type

--- app.js
+++ app.js
@@ -2,16 +2,43 @@ import {ApolloServer, gql} from 'apollo-server';
 import bodyParser from 'body-parser';
 import { buildSchema } from 'graphql';
 
+// const resolvers = {
+//   Query: {
+//     name: () => 'My first product',
+//     description: () => 'This is my first product',
+//     quantity: () => 10,
+//     price: () => 10.99,
+//     onSale: () => true
+//   }
+// };
+
 const typeDefs = gql`
   type Query {
     hello: [String!]!
+    products: [Product!]!
+  }
+
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean!
   }`;
 
 const resolvers = {
   Query: {
-    hello: () => ['Hello','world!']
+    hello: () => ['Hello', 'world!'],
+    products: () => [{
+      name: "product",
+      description: "product description",
+      quantity: 1,
+      price: 9.99,
+      onSale: true
+    }]
   }
 };
+
 const server = new ApolloServer(
   {
     typeDefs,
